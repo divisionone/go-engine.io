c3a17964282f3a059d7d65a870be24107b967b89
SteveOC <steven.oconnor@neds.com>
2019-09-23 10:47:36 +1000

    remove commented out code as per code review comments

--- server_conn.go
+++ server_conn.go
@@ -78,7 +78,6 @@ type serverConn struct {
 	pingTimeout     time.Duration
 	pingInterval    time.Duration
 	pingChan        chan bool
-	//pingLocker      sync.Mutex
 }
 
 var InvalidError = errors.New("invalid transport")
@@ -224,8 +223,6 @@ func (c *serverConn) OnPacket(r *parser.PacketDecoder) {
 		c.writerLocker.Unlock()
 		fallthrough
 	case parser.PONG:
-		//c.pingLocker.Lock()
-		//defer c.pingLocker.Unlock()
 		if s := c.getState(); s != stateNormal && s != stateUpgrading {
 			return
 		}
@@ -270,9 +267,7 @@ func (c *serverConn) OnClose(server transport.Server) {
 	}
 	c.setState(stateClosed)
 	safeRun(func() { close(c.readerChan) })
-	//c.pingLocker.Lock()
 	safeRun(func() { close(c.pingChan) })
-	//c.pingLocker.Unlock()
 	c.callback.onClose(c.id)
 }
 
